141c0258f041c4261023fa436606769072630a7e
Yujie_Liu <136684976+RCrobotcat@users.noreply.github.com>
2026-02-23 01:07:23 +0800

    modified

--- main.cpp
+++ main.cpp
@@ -97,7 +97,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "PhysicsEngineDemo", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
